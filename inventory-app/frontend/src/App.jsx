6258f4afe986e3df65cca9f37094e511ba299169
hemalatha <thutahemalatha@gmail.com>
2026-04-09 16:20:10 +0530

    added option value

--- inventory-app/frontend/src/App.jsx
+++ inventory-app/frontend/src/App.jsx
@@ -1,48 +1,61 @@
 import { useState } from "react";
 
-function App(){
+function App() {
   const [filters, setFilters] = useState({
-    q : "",
-    category:"",
-    minPrice:"",
-    maxPrice:""
+    q: "",
+    category: "",
+    minPrice: "",
+    maxPrice: "",
   });
 
-  const[results , setResults] = useState([]);
-  const search = async()=>{
+  const [results, setResults] = useState([]);
+  const search = async () => {
     const query = new URLSearchParams(filters).toString();
-    const res = await fetch(`https://assignments-backend.onrender.com/search?${query}`);
+    const res = await fetch(
+      `https://assignments-backend.onrender.com/search?${query}`,
+    );
     const data = await res.json();
     setResults(data);
- };
-  return(
+  };
+  return (
     <div>
       <h1>Inventory Search</h1>
-      <input placeholder="Search"
-      onChange={e =>setFilters({...filters, q: e.target.value})}
-       />
-       <input placeholder="Category"
-      onChange={e =>setFilters({...filters, q: e.target.value})}
-       />
-       <input type="number" placeholder="Min Price"
-      onChange={e =>setFilters({...filters, minPrice: e.target.value})}
-       />
-       <input type="number" placeholder="Max Price"
-      onChange={e =>setFilters({...filters, maxPrice: e.target.value})}
-       />
-       
-       <button onClick={search}>Search</button>
-        
-        {results.length === 0 ? (
-          <p>No results found</p>
-        ) : (
-          results.map(item =>(
-            <div key={item.id}>{item.product_name} - ₹{item.price}</div>
-          ))
-        )}
+      <input
+        placeholder="Search"
+        onChange={(e) => setFilters({ ...filters, q: e.target.value })}
+      />
+      <select
+        onChange={(e) => setFilters({ ...filters, category: e.target.value })}
+      >
+        <option value="">All</option>
+        <option value="grain">Grain</option>
+        <option value="electronics">Electronics</option>
+        <option value="clothes">Clothes</option>
+      </select>
 
+      <input
+        type="number"
+        placeholder="Min Price"
+        onChange={(e) => setFilters({ ...filters, minPrice: e.target.value })}
+      />
+      <input
+        type="number"
+        placeholder="Max Price"
+        onChange={(e) => setFilters({ ...filters, maxPrice: e.target.value })}
+      />
+
+      <button onClick={search}>Search</button>
+
+      {results.length === 0 ? (
+        <p>No results found</p>
+      ) : (
+        results.map((item) => (
+          <div key={item.id}>
+            {item.product_name} - ₹{item.price}
+          </div>
+        ))
+      )}
     </div>
-  )
-
+  );
 }
 export default App;
